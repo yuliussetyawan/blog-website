5e3bc95d423e77ea8701b68a24aa14f1df0a47ad
yuliussetyawan <yuliusetyawan@gmail.com>
2023-02-26 23:48:21 +0700

    Challenge 11-Redirect to home page and console log the posts

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const posts = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -16,6 +18,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 app.get("/", (req, res) => {
+  console.log(posts);
   res.render("home", { startingContent: homeStartingContent });
 });
 
@@ -31,13 +34,16 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", (req,res) => {
+app.post("/compose", (req, res) => {
   const post = {
-    title : req.body.postTitle,
-    content : req.body.postBody
-  }
-  console.log(post);
-})
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+
+  res.redirect("/");
+  
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
